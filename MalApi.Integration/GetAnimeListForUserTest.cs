42cf9164145a8c410fa4e47207e88c27ad1e85cd
Greg Najda <gregnajda@gmail.com>
2012-08-21 12:26:17 -0400

    Added integration test project with basic smoke test for GetAnimeListForUser().

---
+++ MalApi.Integration/GetAnimeListForUserTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using MalApi;
+
+namespace MalApi.Integration
+{
+    [TestFixture]
+    public class GetAnimeListForUserTest
+    {
+        [Test]
+        public void GetAnimeListForUser()
+        {
+            string username = "lordhighcaptain";
+            using (MyAnimeListApi api = new MyAnimeListApi())
+            {
+                MalUserLookupResults userLookup = api.GetAnimeListForUser(username);
+                
+                // Just a smoke test that checks that getting an anime list returns something
+                Assert.That(userLookup.AnimeList, Is.Not.Empty);
+            }
+        }
+    }
+}
