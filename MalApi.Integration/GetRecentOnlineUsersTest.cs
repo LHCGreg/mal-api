5676214da926f52b9eac07350336fc35fdbc7634
Greg Najda <gregnajda@gmail.com>
2012-12-09 23:10:54 -0500

    Added integration test for getting recent online users.

---
+++ MalApi.Integration/GetRecentOnlineUsersTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace MalApi.Integration
+{
+    [TestFixture]
+    public class GetRecentOnlineUsersTest
+    {
+        [Test]
+        public void GetRecentOnlineUsers()
+        {
+            using (MyAnimeListApi api = new MyAnimeListApi())
+            {
+                RecentUsersResults results = api.GetRecentOnlineUsers();
+                Assert.That(results.RecentUsers.Count, Is.GreaterThan(0));
+            }
+        }
+    }
+}
