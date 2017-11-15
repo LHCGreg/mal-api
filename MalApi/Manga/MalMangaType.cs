fbafe1592a7f9568058b98316e038e46da9c6605
Shavarsh Movsesyan <naps250@gmail.com>
2017-11-15 12:56:58 +0200

    Missed to remove the comments.

--- MalApi/Manga/MalMangaType.cs
+++ MalApi/Manga/MalMangaType.cs
@@ -6,9 +6,9 @@
         Manga = 1,
         Novel = 2,
         OneShot = 3,
-        Doujin = 4, // not sure
+        Doujin = 4,
         Manwha = 5,
         Manhua = 6,
-        OEL = 7 // not sure
+        OEL = 7
     }
 }
